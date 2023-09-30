bfaeb30303d1495a1bcdaff67b7a299bfb24dfda
BinhoAtaíde <binhoataide14@hotmail.com>
2023-09-30 14:11:44 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./componentes/logo.png")
+  } else {
+    img.setAttribute("src", "./componentes/logo-light.png")
+  }
+}
